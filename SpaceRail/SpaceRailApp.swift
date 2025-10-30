13dbc0d12bfae272eebc0bfa095d1c38ef8ffed4
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-10-30 11:04:10 +0100

    Initial Commit

---
+++ SpaceRail/SpaceRailApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceRailApp.swift
+//  SpaceRail
+//
+//  Created by János Orcsik on 2025. 10. 30..
+//
+
+import SwiftUI
+
+@main
+struct SpaceRailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
